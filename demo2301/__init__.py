0160859e85e483e5b0c47d60fe803dc36e29e279
Pete R Jemian <prjemian@gmail.com>
2023-01-03 12:59:37 -0600

    initial python code

---
+++ demo2301/__init__.py
@@ -0,0 +1,7 @@
+# from importlib.metadata import version, PackageNotFoundError
+
+# try:
+#     __version__ = version("demo2023")
+# except PackageNotFoundError:
+#     # package is not installed
+#     pass
---
+++ demo2301/proof.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+"""
+Try out pydata-sphinx-theme and its version switcher dropdown.
+"""
+
+def main():
+    """
+    The ``main()`` function in this module.
+    """
+    print(f"Here we are in '{__file__}:main()'")
+
+
+if __name__ == "__main__":
+    main()
