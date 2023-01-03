9ba5434a136a94ef24693e2f4c503b3fae3abe7b
Pete R Jemian <prjemian@gmail.com>
2023-01-03 14:29:43 -0600

    ENH add 2nd function

--- demo2301/proof.py
+++ demo2301/proof.py
@@ -2,14 +2,27 @@
 
 """
 Try out pydata-sphinx-theme and its version switcher dropdown.
+
+.. autosummary::
+   ~main
+   ~about
 """
+
+
+def about():
+    """
+    Print something.
+    """
+    print(f"Here we are in '{__file__}:main()'")
 
 
 def main():
     """
     The ``main()`` function in this module.
+
+    Calls ``about()``.
     """
-    print(f"Here we are in '{__file__}:main()'")
+    about()
 
 
 if __name__ == "__main__":
